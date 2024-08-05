174f84ff30ad58595fb56d5c89a0b00fcb6f2a17
FreezeCr1tix <168299466+FreezeCr1tix@users.noreply.github.com>
2024-08-06 00:12:35 +0300

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+class APIException(Exception):
+    pass
+
+class CurrencyConverter:
+    keys = {
+        'DOLLAR': 'USD', 
+        'EURO': 'EUR', 
+        'RUBLE': 'RUB' 
+    }
+
+    @staticmethod
+    def get_price(base, quote, amount):
+        try:
+            base_key = CurrencyConverter.keys[base.upper()]
+        except KeyError:
+            raise APIException(f'Value {base} not found.')
+
+        try:
+            quote_key = CurrencyConverter.keys[quote.upper()]
+        except KeyError:
+            raise APIException(f'Value {quote} not found.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException('Incorrect value amount.')
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/0c345af3d6e93e952c89a366/pair/{base_key}/{quote_key}/{amount}')
+        resp = json.loads(r.content)
+        rate=resp["conversion_rate"]
+        result = rate * amount
+        return result
---
+++ main.py
@@ -0,0 +1,41 @@
+import telebot # type: ignore
+from extensions import APIException, CurrencyConverter
+import configparser
+
+bot = telebot.TeleBot('7484500925:AAEFRewv25yYgcJoYtFxrzcQK9Vdse1DazY')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    text = """I am a bot that can show you the price of a certain amount of currency.
+To find out the price, write me a message in the format:
+<first value, whose price you want to know> <second value, /
+in which you want to know the price> <amount of the first valute>
+For example: USD EUR 100
+Also, you can use comands:
+/values - show list of all availible values"""
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def show_values(message):
+    text = 'Availible values:'
+    for cur in CurrencyConverter.keys:
+        text += f'\n{cur}'
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text'])
+def convert(message):
+    try:
+        base, quote, amount = message.text.split()
+    except ValueError:
+        bot.reply_to(message, 'Invalid input format. Try again.')
+        return
+
+    try:
+        result = CurrencyConverter.get_price(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Error: {e}')
+    else:
+        text = f'Price {amount} {base} in {quote} - {result}'
+        bot.reply_to(message, text)
+bot.polling(non_stop=True)
+
